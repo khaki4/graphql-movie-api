8ad95bdc81cc7494d1f62977ecddc67423d12f05
pi3rrot <4dakto@gmail.com>
2019-07-13 21:32:47 +0900

    add fetch api with yts.lt/api

--- graphql/db.js
+++ graphql/db.js
@@ -1,47 +1,17 @@
-import _ from "underscore";
-let movies = [
-  {
-    id: 0,
-    name: "Star Wars - The new one",
-    score: 1
-  },
-  {
-    id: 1,
-    name: "Avengers - The new one",
-    score: 8
-  },
-  {
-    id: 2,
-    name: "The Godfather I",
-    score: 99
-  },
-  {
-    id: 3,
-    name: "Logan",
-    score: 2
+import fetch from 'node-fetch';
+
+const API_URL = "https://yts.lt/api/v2/list_movies.json";
+
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  const getPrefix = () => REQUEST_URL.includes('?') ? '&' : '?';
+  if (limit > 0) {
+    REQUEST_URL += `${getPrefix()}limit=${limit}`;
   }
-];
-
-export const getMovies = () => movies;
-
-export const getById = id => {
-  const filterdMovies = movies.find(movie => id === movie.id);
-  return filterdMovies;
-};
-
-export const deleteMovie = id => {
-  const cleanedMovies = _.reject(movies, movie => id === movie.id);
-  return movies.length > cleanedMovies.length
-    ? ((movies = cleanedMovies), true)
-    : false;
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: getMovies().length,
-    name,
-    score
-  };
-  movies.push(newMovie);
-  return newMovie;
-};
+  if (rating > 0) {
+    REQUEST_URL += `${getPrefix()}minimum_rating=${rating}`
+  }
+  return fetch(REQUEST_URL)
+  .then(res => res.json())
+  .then(json => json.data.movies);
+}
